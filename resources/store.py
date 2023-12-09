07ac9036a33575c1bf490ac840ee8aae90c9f0a5
Ayaskanta <ayaskantasamal@Ayaskantas-MacBook-Pro.local>
2023-12-09 09:46:36 -0500

    [12-09-2023] - Python api

---
+++ resources/store.py
@@ -0,0 +1,45 @@
+import uuid
+from flask import jsonify, request
+from flask.views import MethodView;
+from flask_smorest import Blueprint,abort
+# from db import stores
+from schemas import StoreSchema
+
+blp =Blueprint("stores",__name__,description="Operation on stores")
+
+@blp.route("/store/<string:store_id>")
+class Store(MethodView):
+    @blp.response(200,StoreSchema)
+    def get(self,store_id):
+        try:
+            return stores[store_id]
+        except:
+            abort(404, message="store not found.")
+            
+    def delete(self,store_id):
+        try:
+            del stores[store_id]
+            return({"message":"Store deleted"})
+        except KeyError:
+            abort(404,message ="Store not found") 
+
+@blp.route("/store")
+class StoreList(MethodView):
+    @blp.response(200,StoreSchema(many=True))
+    def get(self):
+         return stores.values()
+     
+    @blp.arguments(StoreSchema)
+    @blp.response(201,StoreSchema) 
+    def post(self,store_data):
+        if("name" not in store_data):
+            abort(400, message="invalid request. Ensure 'name' is provided.")
+        for store in stores.values():
+            if(store_data["name"] == store["name"]):
+                abort(400, message="store already exists.")
+    
+        store_id = uuid.uuid4().hex
+        store={**store_data, "id": store_id}
+        stores[store_id] = store
+        return store, 201 
+    
